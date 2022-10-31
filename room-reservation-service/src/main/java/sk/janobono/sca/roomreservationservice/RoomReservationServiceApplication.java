33c13f766fb66fef6e4a3466cf80898b88ff3a04
janobono <janobono@gmail.com>
2022-10-31 19:52:29 +0100

    versions bump up
    Signed-off-by: janobono <janobono@gmail.com>

--- room-reservation-service/src/main/java/sk/janobono/sca/roomreservationservice/RoomReservationServiceApplication.java
+++ room-reservation-service/src/main/java/sk/janobono/sca/roomreservationservice/RoomReservationServiceApplication.java
@@ -2,13 +2,12 @@ package sk.janobono.sca.roomreservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import sk.janobono.sca.roomreservationservice.config.ConfigProperties;
 
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages = "sk.janobono.sca.roomreservationservice")
-@EnableConfigurationProperties(ConfigProperties.class)
+@ConfigurationPropertiesScan(basePackages = "sk.janobono.sca.roomreservationservice")
 public class RoomReservationServiceApplication {
 
     public static void main(String[] args) {
